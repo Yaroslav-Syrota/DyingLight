f9ea85764aa69850c6ac63611bbaa8371954ae66
Yaroslav-Syrota <syrota.yaroslav@gmail.com>
2015-10-02 11:48:59 +0300

    create MainActivity, add description.txt for the description of the project

---
+++ app/src/main/java/ua/com/thinkmobiles/dyinglight/MainActivity.java
@@ -0,0 +1,15 @@
+package ua.com.thinkmobiles.dyinglight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 02.10.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
